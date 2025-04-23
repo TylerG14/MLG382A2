4b71b5d4871c17abd0ba27fa35f06faa9ece27b9
MarcoReiners <marcoreiners816@gmail.com>
2025-04-23 15:33:51 +0200

    ygul

--- App.py
+++ App.py
@@ -89,5 +89,6 @@ def predict(n_rf, n_xgb, n_logreg, *values):
 
 # Run app
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(debug=True, host="0.0.0.0", port=port)
